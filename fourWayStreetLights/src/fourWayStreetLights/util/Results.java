b345f1671c3016d97121f0d0d218622f5f7a42b1
amit031190 <akumar22@binghamton.edu>
2018-06-25 16:40:20 -0400

    implemented Results.java to save and prit result in file and stdout

--- fourWayStreetLights/src/fourWayStreetLights/util/Results.java
+++ fourWayStreetLights/src/fourWayStreetLights/util/Results.java
@@ -15,4 +15,63 @@ import java.util.Arrays;
  */
 public class Results implements FileDisplayInterface, StdoutDisplayInterface {
 
+    private String outFileNamePrivate;
+    private StringBuilder sb;
+
+    public Results(String outFileName) {
+        outFileNamePrivate = outFileName;
+        sb = new StringBuilder();
+    }
+
+    /**
+     * append new Result in string container
+     *
+     * return void
+     */
+    public void storeNewResult(String newResult) {
+        sb.append(newResult).append("\n");
+    }
+
+    /**
+     * calls the function to print result from container to file and output
+     * display
+     *
+     * return void
+     */
+    public void printResult() {
+        writeToFile(sb.toString());
+        writeToStdout(sb.toString());
+    }
+
+    /**
+     * write the content stored in Result to output file
+     *
+     * return void
+     * @param s
+     */
+    @Override
+    public void writeToFile(String s) {
+        PrintWriter pW = null;
+        try {
+            pW = new PrintWriter(outFileNamePrivate);
+            pW.write(sb.toString());
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.toString() + Arrays.toString(ex.getStackTrace()));
+        } finally {
+            if (pW != null) {
+                pW.close();
+            }
+        }
+    }
+
+    /**
+     * display the content stored in Result on output screen
+     *
+     * return void
+     * @param s
+     */
+    @Override
+    public void writeToStdout(String s) {
+        System.out.println(sb.toString());
+    }
 }
